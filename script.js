06da227f20185f2c1a939303268f3187d0926548
Raniya <raniyaak4@gmail.com>
2025-07-15 09:48:11 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+const increase = document.getElementById("increase");
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+decrease.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+reset.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+increase.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+
+}
+
+
