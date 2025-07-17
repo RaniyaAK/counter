967dda338da6b35b3b1945ab0bf7ee976fc541cf
Raniya <raniyaak4@gmail.com>
2025-07-16 23:12:34 -0700

    second commit

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 const decrease = document.getElementById("decrease");
 const reset = document.getElementById("reset");
 const increase = document.getElementById("increase");
-const countLabel = document.getElementById("countLabel");
+
+const counterInput = document.getElementById("counter-value")
+
+
+function handleOnchange() {
+    count = counterInput.value
+}
+function changeColor(){
+    if (count > 0){
+        counterInput.style.color = "green";
+    }else if(count < 0){
+        counterInput.style.color = "red";
+    }else {
+        counterInput.style.color = "black";
+    }   
+}
 
 let count = 0;
 
 decrease.onclick = function(){
     count--;
-    countLabel.textContent = count;
-}
+    counterInput.value = count
+    console.log(counterInput.value)  
+    changeColor();   
+}   
 
 reset.onclick = function(){
-    count--;
-    countLabel.textContent = count;
-}
+    count = 0;
+    counterInput.value = count
+    console.log(counterInput.value)
+    changeColor();    
+}    
 
 increase.onclick = function(){
     count++;
-    countLabel.textContent = count;
-
+    counterInput.value = count
+    console.log(counterInput.value)
+    changeColor(); 
 }
 
 
